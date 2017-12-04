136b6a96918348ee6c136ff12180af3bebd8192e
Thaynner Bastos <th.arbastos@gmail.com>
2017-12-04 14:46:44 -0200

    Project fixed

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -7,32 +7,34 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
 
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = this._context.Customers.ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
                 return HttpNotFound();
             }
             return View(customer);
-        }
-
-        public IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "Jhon Smith"},
-                new Customer { Id = 2, Name = "Mary Williams"}
-            };
         }
     }
 }
