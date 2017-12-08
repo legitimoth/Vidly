225f032a40d0b4f5f37af70d0f867403d8df0dc8
Thaynner Bastos <th.arbastos@gmail.com>
2017-12-08 13:58:01 -0200

    The whole project until the 3rd exercise.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -7,28 +8,35 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        
+        public ApplicationDbContext _context { get; set; }
+
+        public MoviesController()
+        {
+            this._context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            this._context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = this._context.Movies.Include( m => m.Genre ).ToList();
             return View(movies);
         }
 
         public ActionResult Details(int id)
         {
-            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
-            return View(movie);
-        }
+            var movies = this._context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
 
-        public IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
+            if (movies == null)
             {
-                new Movie { Id = 1, Name = "Shrek"},
-                new Movie { Id = 2, Name = "Wall-e"}
-            };
-        }
+                return HttpNotFound();
+            }
 
+            return View(movies);
+        }
 
     }
 }
